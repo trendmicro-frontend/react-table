b46866330274b9a345f2bb152ed5dee420b9fdfe
Cheton Wu <cheton@gmail.com>
2017-03-03 23:36:44 +0800

    Add TablePagination component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { ColumnGroup, Column } from 'rc-table';
 import Table from './Table';
 import TableToolbar from './TableToolbar';
+import TablePagination from './TablePagination';
 
 export { ColumnGroup, Column };
 
 Table.Toolbar = TableToolbar;
+Table.Pagination = TablePagination;
 Table.ColumnGroup = ColumnGroup;
 Table.Column = Column;
 
