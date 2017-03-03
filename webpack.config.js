f8370aa597903fb2d7f01e35185889dcbdbe983d
Cheton Wu <cheton@gmail.com>
2017-03-03 20:01:02 +0800

    Inline svg images

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
             },
             {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader'
+                loader: 'url-loader'
             }
         ]
     },
