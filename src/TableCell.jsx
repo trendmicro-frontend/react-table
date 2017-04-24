d665aea775bad3199783b48546b3bf879f0d6a12
Tina C Lin (RD-TW) <tina_c_lin@trend.com.tw>
2017-04-24 17:00:43 +0800

    Use lodash.get to get object's value.

--- src/TableCell.jsx
+++ src/TableCell.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import get from 'lodash.get';
 import styles from './index.styl';
 
 class TableCell extends PureComponent {
@@ -17,7 +18,7 @@ class TableCell extends PureComponent {
         const { column, record } = this.props;
         const cName = column.dataIndex;
         const render = column.render;
-        let text = record[cName];
+        let text = get(record, cName);
         return (
             <div className={styles.td}>
                 <div className={styles.tdContent}>
